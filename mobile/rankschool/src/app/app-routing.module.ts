e361881fce20a69c90314548e0a684341dd3c7cb
NatanTavares <nattantavares.s15@gmail.com>
2020-09-26 16:08:44 -0300

    Home page base structure

--- mobile/rankschool/src/app/app-routing.module.ts
+++ mobile/rankschool/src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'sing-up',
+    loadChildren: () => import('./pages/sing-up/sing-up.module').then( m => m.SingUpPageModule)
   }
 ];
 
