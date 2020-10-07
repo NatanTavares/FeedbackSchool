33cdba9953c0ef0d4084684c900dafc5597d51b9
NatanTavares <nattantavares.s15@gmail.com>
2020-10-06 23:58:24 -0300

    Trying to use the component (Header) - Just trying XD

--- mobile/rankschool/src/app/app.module.ts
+++ mobile/rankschool/src/app/app.module.ts
@@ -8,9 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent, 
+    HeaderComponent
+  ],
   entryComponents: [],
   imports: [
     BrowserModule,
