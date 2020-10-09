f4cbc2ee1c716c5b9e67e97cae7831a62303d75d
Natan <nattantavares.s15@gmail.com>
2020-10-09 19:59:33 -0300

    Exit component

---
+++ mobile/rankschool/src/app/components/exit/exit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exit',
+  templateUrl: './exit.component.html',
+  styleUrls: ['./exit.component.scss'],
+})
+export class ExitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
