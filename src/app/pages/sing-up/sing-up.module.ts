0090b39d5dc0911fbb1e5504ba4351c1befa8867
Natan <nattantavares.s15@gmail.com>
2020-11-24 04:47:10 -0300

    Sign up

---
+++ src/app/pages/sing-up/sing-up.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SingUpPageRoutingModule } from './sing-up-routing.module';
+
+import { SingUpPage } from './sing-up.page';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    SingUpPageRoutingModule
+  ],
+  declarations: [
+    SingUpPage,
+    HeaderComponent
+  ]
+})
+export class SingUpPageModule {}
---
+++ src/app/pages/sing-up/sing-up.page.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController, MenuController, ToastController } from '@ionic/angular';
+import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-sing-up',
+  templateUrl: './sing-up.page.html',
+  styleUrls: ['./sing-up.page.scss'],
+})
+export class SingUpPage implements OnInit {
+  public userRegister: User = {}
+  private loading: any;
+
+  constructor(
+    private _menu: MenuController,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    
+  }
+
+
+  async register() {
+    await this.presentLoading();
+
+    try {
+      await this.authService.register(this.userRegister);
+      this.router.navigate(['/evaluate'])
+
+    } catch (error) {
+      console.error(error)
+      let message: string
+
+      switch (error.code) {
+        case 'auth/email-already-in-use':
+          message = 'E-mail já sendo usado!'
+          break;
+        case 'auth/invalid-email':
+          message = 'E-mail inválido!'
+          break;
+        case 'auth/argument-error':
+          message = 'Algum campo vazio!'
+          break;
+        case 'auth/weak-password':
+          message = 'Senha fraca!'
+          break;
+      }
+
+      this.presentToast(message);
+    } finally {
+      this.loading.dismiss();
+    }
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingCtrl.create({ message: 'Porfavor, aguarde...' });
+    return this.loading.present();
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({ message, duration: 2000 });
+    toast.present();
+  }
+
+  async openMenu() {
+    await this._menu.open();
+  }
+
+
+
+}
